329efe8053a874c57535f14796acd82e7f3d48fc
yuzelli <zsl5155@qq.com>
2017-08-01 14:57:43 +0800

    add blue tooch

--- app/src/main/java/com/example/yuzelli/bluetoolsvehiclemonitoring/view/activity/DeviceSetActivity.java
+++ app/src/main/java/com/example/yuzelli/bluetoolsvehiclemonitoring/view/activity/DeviceSetActivity.java
@@ -2,11 +2,34 @@ package com.example.yuzelli.bluetoolsvehiclemonitoring.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yuzelli.bluetoolsvehiclemonitoring.R;
 import com.example.yuzelli.bluetoolsvehiclemonitoring.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class DeviceSetActivity extends BaseActivity {
+
+    @BindView(R.id.iv_left)
+    ImageView ivLeft;
+    @BindView(R.id.tv_center)
+    TextView tvCenter;
+    @BindView(R.id.tv_right)
+    TextView tvRight;
+    @BindView(R.id.tv_blue_setting)
+    TextView tvBlueSetting;
+    @BindView(R.id.tv_add_device)
+    TextView tvAddDevice;
+    @BindView(R.id.tv_remove_device)
+    TextView tvRemoveDevice;
+    @BindView(R.id.tv_change_device)
+    TextView tvChangeDevice;
 
     @Override
     protected int layoutInit() {
@@ -23,8 +46,29 @@ public class DeviceSetActivity extends BaseActivity {
 
     }
 
-    public static void startAction(Context context){
-        Intent intent = new Intent(context,DeviceSetActivity.class);
+    public static void startAction(Context context) {
+        Intent intent = new Intent(context, DeviceSetActivity.class);
         context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tv_blue_setting, R.id.tv_add_device, R.id.tv_remove_device, R.id.tv_change_device})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_blue_setting:
+                break;
+            case R.id.tv_add_device:
+                break;
+            case R.id.tv_remove_device:
+                break;
+            case R.id.tv_change_device:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/yuzelli/bluetoolsvehiclemonitoring/view/fragment/SetFragment.java
+++ app/src/main/java/com/example/yuzelli/bluetoolsvehiclemonitoring/view/fragment/SetFragment.java
@@ -1,16 +1,29 @@
 package com.example.yuzelli.bluetoolsvehiclemonitoring.view.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yuzelli.bluetoolsvehiclemonitoring.R;
 import com.example.yuzelli.bluetoolsvehiclemonitoring.base.BaseFragment;
+import com.example.yuzelli.bluetoolsvehiclemonitoring.utils.BluetoothChatUtil;
+
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,6 +71,66 @@ public class SetFragment extends BaseFragment {
     ImageView imgEmpty;
     @BindView(R.id.rl_empty)
     RelativeLayout rlEmpty;
+    @BindView(R.id.btn_blth_connect)
+    Button btn_blth_connect;
+    @BindView(R.id.tv_connect_state)
+    TextView tv_connect_state;
+
+
+    private final static String TAG = "SetFragment";
+    //设置绑定的蓝牙名称
+    public static final String BLUETOOTH_NAME = "Che2-TL00M";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
+    private Context mContext;
+
+    private ProgressDialog mProgressDialog;
+    private BluetoothChatUtil mBlthChatUtil;
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothChatUtil.STATE_CONNECTED:
+                    String deviceName = msg.getData().getString(BluetoothChatUtil.DEVICE_NAME);
+                    tv_connect_state.setText("已成功连接到设备" + deviceName);
+                    if (mProgressDialog.isShowing()) {
+                        mProgressDialog.dismiss();
+                    }
+                    break;
+                case BluetoothChatUtil.STATAE_CONNECT_FAILURE:
+                    if (mProgressDialog.isShowing()) {
+                        mProgressDialog.dismiss();
+                    }
+                    showToast("连接失败");
+                    break;
+                case BluetoothChatUtil.MESSAGE_DISCONNECTED:
+                    if (mProgressDialog.isShowing()) {
+                        mProgressDialog.dismiss();
+                    }
+                    tv_connect_state.setText("与设备断开连接");
+                    break;
+                case BluetoothChatUtil.MESSAGE_READ: {
+                    byte[] buf = msg.getData().getByteArray(BluetoothChatUtil.READ_MSG);
+                    String str = new String(buf, 0, buf.length);
+                    //Toast.makeText(getApplicationContext(), "读成功" + str, Toast.LENGTH_SHORT).show();
+                    showToast("读成功" + "-->" + str);
+
+                    break;
+                }
+                case BluetoothChatUtil.MESSAGE_WRITE: {
+                    byte[] buf = (byte[]) msg.obj;
+                    String str = new String(buf, 0, buf.length);
+                    showToast("发送成功");
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+
+        ;
+    };
 
 
     @Override
@@ -69,15 +142,131 @@ public class SetFragment extends BaseFragment {
     protected void bindEvent(View v) {
         ivLeft.setVisibility(View.GONE);
         tvCenter.setText("当前设备");
-        tvRight.setText("设备设置");
+        tvRight.setText("目标设备");
+        tvRight.setVisibility(View.VISIBLE);
+        mContext = getActivity();
+        initBluetooth();
+        mBlthChatUtil = BluetoothChatUtil.getInstance(mContext);
+        mBlthChatUtil.registerHandler(mHandler);
+        mProgressDialog = new ProgressDialog(getActivity());
+    }
 
+
+    private void initBluetooth() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {//设备不支持蓝牙
+            showToast("设备不支持蓝牙");
+            return;
+        }
+        //判断蓝牙是否开启
+        if (!mBluetoothAdapter.isEnabled()) {//蓝牙未开启
+            Intent enableIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            //mBluetoothAdapter.enable();此方法直接开启蓝牙，不建议这样用。
+        }
+        //注册广播接收者，监听扫描到的蓝牙设备
+        IntentFilter filter = new IntentFilter();
+        //发现设备
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        getActivity().registerReceiver(mBluetoothReceiver, filter);
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBlthChatUtil = null;
+        getActivity().unregisterReceiver(mBluetoothReceiver);
+    }
+
+
+    @Override
     protected void fillData() {
+        if (mBlthChatUtil != null) {
+            if (mBlthChatUtil.getState() == BluetoothChatUtil.STATE_CONNECTED) {
+                BluetoothDevice device = mBlthChatUtil.getConnectedDevice();
+                if (null != device && null != device.getName()) {
+                    tv_connect_state.setText("已成功连接到设备" + device.getName());
+                } else {
+                    tv_connect_state.setText("已成功连接到设备");
+                }
+            }
+        }
+    }
+
+
+    private void discoveryDevices() {
+        if (mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+        if (mBluetoothAdapter.isDiscovering()) {
+            //如果正在扫描则返回
+            return;
+        }
+        mProgressDialog.setTitle(getResources().getString(R.string.progress_scaning));
+        mProgressDialog.show();
+        // 扫描蓝牙设备
+        mBluetoothAdapter.startDiscovery();
 
     }
-    @OnClick(R.id.tv_right)
-    public void onViewClicked() {
+
+    private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "mBluetoothReceiver action =" + action);
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //获取蓝牙设备
+                BluetoothDevice scanDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (scanDevice == null || scanDevice.getName() == null) return;
+                Log.d(TAG, "name=" + scanDevice.getName() + "address=" + scanDevice.getAddress());
+                //蓝牙设备名称
+                String name = scanDevice.getName();
+                if (name != null && name.equals(BLUETOOTH_NAME)) {
+                    mBluetoothAdapter.cancelDiscovery(); //取消扫描
+                    mProgressDialog.setTitle(getResources().getString(R.string.progress_connecting));
+                    mBlthChatUtil.connect(scanDevice);
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+            }
+        }
+    };
+
+    @SuppressWarnings("unused")
+    private void getBtDeviceInfo() {
+        //获取本机蓝牙名称
+        String name = mBluetoothAdapter.getName();
+        //获取本机蓝牙地址
+        String address = mBluetoothAdapter.getAddress();
+        Log.d(TAG, "bluetooth name =" + name + " address =" + address);
+        //获取已配对蓝牙设备
+        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+        Log.d(TAG, "bonded device size =" + devices.size());
+        for (BluetoothDevice bonddevice : devices) {
+            Log.d(TAG, "bonded device name =" + bonddevice.getName() +
+                    " address" + bonddevice.getAddress());
+        }
+    }
+
+
+    @OnClick({R.id.tv_right, R.id.btn_blth_connect,R.id.btn_blth_disconnect})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_blth_disconnect:
+                if (mBlthChatUtil.getState() != BluetoothChatUtil.STATE_CONNECTED) {
+                    showToast("蓝牙未连接");
+                }else {
+                    mBlthChatUtil.disconnect();
+                }
+                break;
+            case R.id.btn_blth_connect:
+                if (mBlthChatUtil.getState() == BluetoothChatUtil.STATE_CONNECTED) {
+                    showToast("蓝牙已连接");
+                }else {
+                    discoveryDevices();
+                }
+                break;
+        }
     }
 }
